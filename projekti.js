3033bcf4637561d2bbb6f8b8be58eaa0d8266f02
valtrinacacaj <118756985+valtrinacacaj@users.noreply.github.com>
2023-01-19 12:39:07 +0100

    Add files via upload

--- projekti.js
+++ projekti.js
@@ -49,125 +49,121 @@ setInterval(
   }, 300
 )
 }
+// Funksioni per canvas conf
 
+var canvas = document.querySelector('canvas');
+var context = canvas.getContext('2d');
 
+// Declarations
+var FALL_SPEED;
+var DRIFT_SPEED;
+var ROTATE_SPEED;
+var FLIP_SPEED;
+var CONFETTI_LIMIT;
+var CONFETTI_SIZE;
+var CONFETTI_INTERVAL;
+var RESOLUTION = 1;
+var FPS = 60;
 
-// funksioni per bore ne background
-
-let canvas, ctx, w, h, snows, snows2;
-
-function init() {
-	canvas = document.querySelector("#canvas");
-	ctx = canvas.getContext("2d");
-
-	resizeReset();
-	animationLoop();
+function initialize () {
+  canvas.width = canvas.clientWidth/RESOLUTION;
+  canvas.height = canvas.clientHeight/RESOLUTION;
+  FALL_SPEED = 2*60/FPS;
+  DRIFT_SPEED = 1*60/FPS;
+  ROTATE_SPEED = 0.02*60/FPS;
+  FLIP_SPEED = 0.2*60/FPS;
+  CONFETTI_LIMIT = canvas.width/10;
+  CONFETTI_SIZE = 4/RESOLUTION;
+  CONFETTI_INTERVAL = (canvas.height*1000)/(FALL_SPEED*60*CONFETTI_LIMIT);
 }
 
+initialize();
 
+var confettiList = [];
 
-function resizeReset() {
-	w = canvas.width = window.innerWidth;
-	h = canvas.height = window.innerHeight;
-
-	snows = [];
-	for (let i = 0; i < 500; i++) {
-		snows.push(new Snow());
-	}
-	snows2 = [];
-	for (let i = 0; i < 500; i++) {
-		snows2.push(new Snow2());
-	}
+function Confetti (xPosition, yPosition) {
+  this.yPosition = yPosition || 0;
+  this.xPosition = xPosition || canvas.width*Math.random();
+  this.rotation = 0;
+  this.flip = 0;
+  this.ySpeed = (Math.random()*0.5+0.5)*FALL_SPEED;
+  this.xSpeed = (Math.random()*2-1)*DRIFT_SPEED;
+  this.rotationSpeed = (Math.random()*2-1)*ROTATE_SPEED;
+  this.flipSpeed = (Math.random()*2-1)*FLIP_SPEED;
+  this.hue = Math.random()*360;
 }
 
-function animationLoop() {
-	ctx.clearRect(0, 0, w, h);
-	drawScene();
-	requestAnimationFrame(animationLoop);
+Confetti.prototype.step = function () {
+  this.yPosition += this.ySpeed;
+  this.xPosition += this.xSpeed;
+  this.rotation += this.rotationSpeed;
+  this.flip += this.flipSpeed;
 }
 
-function drawScene() {
-	for (let i = 0; i < snows.length; i++) {
-		snows[i].update();
-		snows[i].draw();
-	}
-	for (let i = 0; i < snows2.length; i++) {
-		snows2[i].update();
-		snows2[i].draw();
-	}
+Confetti.prototype.visible = function () {
+  return this.xPosition > 0 && this.yPosition > 0 && this.xPosition < canvas.width && this.yPosition < canvas.height;
 }
 
-function getRandomInt(min, max) {
-	return Math.round(Math.random() * (max - min)) + min;
+Confetti.prototype.color = function () {
+  alpha = (1-(this.yPosition/canvas.height));
+  saturation = (Math.pow(Math.sin(this.flip), 20)*0.5+0.5)*100
+  return "hsla(" + this.hue + ", 100%, " + saturation + "%, " + alpha + ")";
 }
 
-class Snow {
-	constructor() {
-		this.reset();
-		this.rgb = "52, 235, 236"; // change snow color
-	}
-	reset() {
-		this.x = getRandomInt(0, w);
-		this.xc = ((this.x - (w / 2)) / (w / 2)) / 2;
-		this.y = getRandomInt(-(h * 0.3), h);
-		this.yc = getRandomInt(10, 15) / 10;
-		this.size = getRandomInt(10, 20) / 10;
-		this.a = getRandomInt(-10, 0) / 10;
-		this.ac = getRandomInt(3, 5) / 100;
-	}
-	draw() {
-		ctx.beginPath(); // since canvas works only with rectangles and paths
-		ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-		ctx.fillStyle = `rgba(${this.rgb}, ${this.a})`;
-		ctx.strokeStyle = `rgba(${this.rgb}, ${this.a})`;
-		ctx.fill();
-		ctx.stroke();
-		ctx.closePath();
-	}
-	update() {
-		this.x += this.xc;
-		this.y += this.yc;
-		this.a += this.ac;
-		if (this.a > 2) {
-			this.ac *= -1;
-		} else if (this.a < 0 && this.ac < 0) {
-			this.reset();
-		}
-	}
+Confetti.prototype.draw = function () {
+  context.fillStyle = this.color();
+  context.resetTransform();
+  context.translate(this.xPosition+CONFETTI_SIZE/2, this.yPosition+CONFETTI_SIZE/2);
+  context.rotate(Math.PI*2*this.rotation);
+  context.scale(Math.sin(this.flip), 1);
+  context.fillRect(-CONFETTI_SIZE/2, -CONFETTI_SIZE/2, CONFETTI_SIZE, CONFETTI_SIZE);
 }
 
-class Snow2 {
-	constructor() {
-		this.reset();
-		this.rgb = "52, 235, 236";
-	}
-	reset() {
-		this.x = getRandomInt(0, w);
-		this.y = getRandomInt(0, h);
-		this.size = getRandomInt(0, 5) / 10;
-		this.a = getRandomInt(-10, 0) / 10;
-		this.ac = 0.01;
-	}
-	draw() {
-		ctx.beginPath();
-		ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-		ctx.fillStyle = `rgba(${this.rgb}, ${this.a})`;
-		ctx.strokeStyle = `rgba(${this.rgb}, ${this.a})`;
-		ctx.fill();
-		ctx.stroke();
-		ctx.closePath();
-	}
-	update() {
-		this.y -= 0.1;
-		this.a += this.ac;
-		if (this.a > 1.5) {
-			this.ac *= -1;
-		} else if (this.a < 0 && this.ac < 0) {
-			this.reset();
-		}
-	}
+var lastTime = Date.now();
+var lastMakeTime = lastTime;
+var xMousePosition;
+var yMousePosition;
+// Animation Loop
+function animate() {
+  var nextTime = Date.now();
+  requestAnimationFrame(animate);
+  if (nextTime - lastTime < 1000 / FPS ) {
+    return;
+  }
+  lastTime = nextTime;
+  context.resetTransform();
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  if (confettiList.length < CONFETTI_LIMIT && (nextTime - lastMakeTime) > CONFETTI_INTERVAL) {
+    confettiList.push(new Confetti(xMousePosition, yMousePosition));
+    lastMakeTime = nextTime;
+  }
+  confettiList.forEach(function (confetti) {
+    confetti.step();
+    confetti.draw();
+  });
+  confettiList = confettiList.filter(function (confetti) {
+    return confetti.visible();
+  });
 }
 
-window.addEventListener("DOMContentLoaded", init);
-window.addEventListener("resize", resizeReset);
+function mouseDraw (event) {
+  xMousePosition = event.clientX;
+  yMousePosition = event.clientY;
+}
+
+canvas.addEventListener('mousedown', function (event) {
+  canvas.addEventListener('mousemove', mouseDraw);
+  xMousePosition = event.clientX;
+  yMousePosition = event.clientY;
+})
+
+canvas.addEventListener('mouseup', function (event) {
+  canvas.removeEventListener('mousemove', mouseDraw);
+  xMousePosition = null;
+  yMousePosition = null;
+})
+
+window.addEventListener('resize', initialize);
+
+animate();
 
